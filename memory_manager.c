15c6c7a320e91690e4a43969070bb15172e273d7
Linnea <148188744+NEAAA4@users.noreply.github.com>
2024-10-21 09:22:50 +0200

    Update memory_manager.c

--- memory_manager.c
+++ memory_manager.c
@@ -5,85 +5,66 @@
 #include <stdint.h>
 #include <stddef.h>
 
-// define poolsize and blocksize
-#define pool_size 1024
-#define block_size 16
 
-char memory_pool[pool_size];
+#define pool_size 1024 // storlek på memory pool
+#define block_size 16 // storlek på memory block
+
+char memory_pool[pool_size]; 
 bool allocated[pool_size/block_size];
 size_t num_blocks;
 
 
 void mem_init(size_t size) {
-    // Allocate memory static
+    // Allocate memory static 
     for (int i = 0; i < pool_size / block_size; i++) {
         allocated[i] = false;
-    }
+    } 
 }
 
 void* mem_alloc(size_t size){
     //allocate in free space
     if (size > block_size) {
-        return NULL;
+        return NULL; 
     }
     for (int i = 0; i < pool_size / block_size; i++) {
         if (!allocated[i]) {
             allocated[i] = true;
-            return (void*)&memory_pool[i * block_size];
-        }
+            return (void*)&memory_pool[i * block_size]; 
+
+        } 
     }
     return NULL;
 }
 
 
 void mem_free(void* block){
-    //free in allocated space
     if (block >= (void*)memory_pool && block < (void*)(memory_pool + pool_size)) {
-        return;
-    }
+        return; 
+    } 
     size_t offset = (unsigned char*)block - (unsigned char*)memory_pool;
-    size_t index = offset / block_size;
+    size_t index = offset / block_size; 
 
     if (index < num_blocks) {
-        allocated[index] = false;
+        allocated[index] = false; 
     }
 }
 
 void* mem_resize(void* block, size_t size) {
-    // resize 
-    if (block == NULL) {
-        return mem_alloc(size);
-    }
-
-    if (size == 0) {
-        mem_free(block);
+    void* new = mem_alloc(size);
+    if (new == NULL) {
         return NULL;
     }
-
-    size_t offset = (unsigned char*)block - (unsigned char*)memory_pool;
-
-    size_t sizeing = 0;
-    for (size_t i = offset; i<pool_size && allocated[i]; i++) {
-        sizeing++;
+    if (block != NULL) {
+        mem_free(block);
     }
-
-    if (sizeing >= size) {
-        return block;
-    }
-
-    for (size_t i = 0; i < sizeing; i++) {
-        ((unsigned char*)blocking[i] = (unsigned char*)block)[i];
-    }
-
-    mem_free(block);
-
-    return blocking;
+    return new;
 }
 
 
 void mem_deinit() {
-    // Delocate memory
+    // Deallocate memory static
     for (int i = 0; i < pool_size / block_size; i++) {
-        allocated[i] = false;
+        allocated[i] = false; 
     }
 }
+
