358f9f9c6a5ec52fe3a822a7a345305786d34a53
Linnea <148188744+NEAAA4@users.noreply.github.com>
2024-10-23 09:47:37 +0200

    TYPEDEF
    New tactic

--- memory_manager.c
+++ memory_manager.c
@@ -5,75 +5,146 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define pool_size 1024
-#define block_size 16
 
-char memory_pool[pool_size];
-bool allocated[pool_size/block_size];
-size_t num_blocks;
+typedef struct Memory {
+    size_t size;          
+    bool free;        
+    struct Memory *next; 
+} Memory;
+
+
+static void *memory_pool = NULL;
+static Memory *lista = NULL;
+
+#define ALIGN sizeof(Memory)
 
 void mem_init(size_t size) {
-    // Allocate memory static 
-    
-    for (int i = 0; i < size / block_size; i++) {
-        allocated[i] = false;
+    if (memory_pool != NULL) {
+        return;
     }
+    
+    memory_pool = malloc(size);
+    if (!memory_pool) {
+        return;
+    }
+    
+    lista = (Memory *) memory_pool;
+    lista->size = size - sizeof(Memory);  
+    lista->free = true;                 
+    lista->next = NULL;                     
 }
 
-void* mem_alloc(size_t size){
-    //allocate in free space
-    if (size > block_size) {
-        return NULL; 
+
+void* mem_alloc(size_t size) {
+    if (size <= 0) {
+        return NULL;
     }
 
-    for (int i = 0; i < pool_size / block_size; i++) {
-        if (!allocated[i]) {
-            allocated[i] = true;
-            return (void*)&memory_pool[i * block_size];
+    size = align_size(size);
+    Memory *here = lista;
+    Memory *bfore = NULL;
+
+    while (here != NULL) {
+        if (here->free && here->size >= size) {
+            if (here->size >= size + sizeof(Memory) + ALIGN) {
+                Memory *new = (Memory *)((char *)here + sizeof(Memory) + size);
+                new->size = here->size - size - sizeof(Memory);
+                new->free = true;
+                new->next = here->next;
+
+                here->size = size;
+                here->free = false;
+                here->next = new;
+
+            } else {                
+                here->free = false;
+            }
+            return (char *)here + sizeof(Memory);
         }
+
+        bfore = here;
+        here = here->next;
     }
     return NULL;
 }
 
 
-
-void mem_free(void* block){
-
+void mem_free(void* block) {
     if (block == NULL) {
-        return;
-    }
-    
-    if (block >= (void*)memory_pool && block < (void*)(memory_pool + pool_size)) {
         return; 
     }
 
-    int index = ((char*)block - (char*)memory_pool) / block_size;
+    Memory *here = (Memory *)((char *)block - sizeof(Memory));
+    if (!block_here->free) {
+        block_here->free = true;
 
-    if (index >= 0 && index < pool_size / block_size && allocated[index]) {
-        allocated[index] = false;
+        Memory *block_next = block_here->next;
+        if (block_next != NULL && block_next->free) {
+            block_here->size += block_next->size + sizeof(Memory);
+            block_here->next = block_next->next; 
+        }
+
+        Memory *block_bfore = lista;
+        while (block_bfore != NULL && block_bfore->next != block_here) {
+            block_bfore = block_bfore->next;
+        }
+
+        if (block_bfore != NULL && block_bfore->free) {
+            block_bfore->size += block_here->size + sizeof(Memory);
+            block_bfore->next = block_here->next; 
+        }
+    } else {
+        return;
     }
-    else {
+}
+
+void* mem_resize(void* block, size_t size) {
+    if (block == NULL) {
+        return mem_alloc(size);  
+    }
+
+    Memory *block_here = (Memory *)((char *)block - sizeof(Memory));
+    if (block_here->free) {
+        return NULL;
+    }
+
+    size = align_size(size);
+    if (size <= block_here->size) {
+        block_here->size = size;
+        return block;
+    }
+
+    Memory *block_next = block_here->next;
+    if (block_next != NULL && block_next->free && (block_here->size + sizeof(Memory) + block_next->size) >= size) {
+        block_here->size += block_next->size + sizeof(Memory);
+        block_here->next = next_block->next; 
+        block_here->free = false; 
+        return block;
+    }
+
+    void* new = mem_alloc(size);
+    if (new == NULL) {
+        return NULL; 
+    }
+
+    memcpy(new, block, block_here->size < size ? block_here->size : size);
+    mem_free(block);
+    return new; 
+}
+
+void mem_deinit() {
+    if (memory_pool == NULL) {
         return;
     }
 
-}
-
-
-void* mem_resize(void* block, size_t size) {
-    void* new = mem_alloc(size);
-    if (new == NULL) {
-        return NULL;
+    Memory *here = lista;
+    while (here != NULL) {
+        if (!here->free) {
+            printf("Freeing allocated block of size %zu at %p\n", current->size, (void *)current);
+        }
+        here = here->next;
     }
-    if (block != NULL) {
-        mem_free(block);
-    }
-    return new;
-}
-
-
-void mem_deinit() {
-    // Deallocate memory static
-    for (int i = 0; i < pool_size / block_size; i++) {
-        allocated[i] = false; 
-    }
+    free(memory_pool);
+    memory_pool = NULL; 
+    lista = NULL;   
 }
