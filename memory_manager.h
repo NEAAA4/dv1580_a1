878e9dc5c0a44f71cf1a0ec68381d9da6d6e55eb
Linnea <148188744+NEAAA4@users.noreply.github.com>
2024-11-28 11:14:00 +0100

    Update memory_manager.h

--- memory_manager.h
+++ memory_manager.h
@@ -8,12 +8,6 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define pool_size 1024 // storlek på memory pool
-#define block_size 16 // storlek på memory block
-
-extern char memory_pool[pool_size]; 
-extern bool allocated[pool_size/block_size];
-extern size_t num_blocks;
 
 void mem_init(size_t size);
 void* mem_alloc(size_t size);
