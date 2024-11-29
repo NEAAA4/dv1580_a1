54ee4dfda70396b9d7cb9d8ebeab849cdcdf2046
Linnea <148188744+NEAAA4@users.noreply.github.com>
2024-11-29 08:37:21 +0100

    Update memory_manager.c

--- memory_manager.c
+++ memory_manager.c
@@ -19,14 +19,14 @@ static size_t pool_size = 0;
 void mem_init(size_t size) {
     memory_pool = (char*)malloc(size);
     if (memory_pool == NULL) {
-        printf("Error: Memory allocation failed\n");
+        printf("Memory allocation failed\n");
         return;
     }
 
     pool_size = size;
     lista = (Memory*)malloc(sizeof(Memory)); // metadata
     if (lista == NULL) {
-        printf("Error: Allocation of block list failed\n");
+        printf("Allocation of block list failed\n");
         return;
     }
 
@@ -37,7 +37,6 @@ void mem_init(size_t size) {
 }
 
 void* mem_alloc(size_t size) {
-
     Memory* here = lista;
     while (here != NULL) {
         if (here->freeing && here->size >= size) {
@@ -48,24 +47,23 @@ void* mem_alloc(size_t size) {
                     return NULL;  
                 }
                 
-                new->starting = here->starting + size; 
+                new->starting = here->starting + size;  // + size to next 
                 new->size = here->size - size;
-                new->freeing = 1;
+                new->freeing = 1;  
                 new->next = here->next;
 
                 here->size = size;
-                here->freeing = 0;
+                here->freeing = 0; // not free
                 here->next = new;
-            } else {
+            } 
+            else {
                 here->freeing = 0;
             }
-
-            return memory_pool + here->starting;
+            return memory_pool + here->starting; // memorypool + startvalue
         }
 
         here = here->next;
     }
-
     return NULL; 
 }
 
@@ -85,22 +83,22 @@ void mem_free(void* block) {
             }
             here->freeing = 1;
 
-            if (here->next && here->next->freeing) {
+            if (here->next && here->next->freeing) { // merge with next block
                 Memory* bnext = here->next;
                 here->size += bnext->size;
                 here->next = bnext->next;
-                free(bnext);
+                mem_free(bnext);
             }
 
-            if (bfore && bfore->freeing) {
+            if (bfore && bfore->freeing) { // merge with prev block
                 bfore->size += here->size;
                 bfore->next = here->next;
-                free(here);
+                mem_free(here);
             }
 
             return;
         }
-        bfore = here;
+        bfore = here; // next block
         here = here->next;
     }
 }
@@ -117,10 +115,10 @@ void* mem_resize(void* block, size_t size) {
     Memory* bhere = (Memory*)((char*)block - sizeof(Memory));
     size_t shere = bhere->size;
 
-    if (shere >= size) {
+    if (shere >= size) { 
         return block;
     } else {
-        void* new = mem_alloc(size);
+        void* new = mem_alloc(size); // allocate new block
         if (new == NULL) {
             printf("Allocation failed for new block \n");
             return NULL;  
@@ -148,4 +146,3 @@ void mem_deinit() {
     }
     lista = NULL;
 }
-
